7c9cf504a088cae5158f550ba282c26a34730bb3
Bruno <b.nadj11@gmail.com>
2026-01-18 10:14:11 +0100

    First example of person

--- Classes.py
+++ Classes.py
@@ -4,4 +4,7 @@ class Person():
         self.age=age
     def info(self):
         print(f"Hello, I am {self.name} and I am {self.age} years old!")
+
+p1=Person("Bruno",24)
+print(p1.info())
         
