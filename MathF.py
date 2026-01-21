4e2a11b552a82b1792895e84d2db3fd658c586d5
Bruno <b.nadj11@gmail.com>
2026-01-21 19:02:52 +0100

    product_arg is added

--- MathF.py
+++ MathF.py
@@ -3,3 +3,12 @@ def sum_arg(*args):
     for arg in args:
         s+=arg
     return s
+
+def product_arg(*args):
+    if len(args)>0:
+        p=1
+        for arg in args:
+            p*=arg
+        return p
+    else:
+        return False
