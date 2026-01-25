c0c8d32c8cb630445a9606d4003a23f9205abc84
Bruno <b.nadj11@gmail.com>
2026-01-25 08:34:19 +0100

    class Student is added

--- Classes.py
+++ Classes.py
@@ -14,3 +14,12 @@ print(p2.info())
 
 p3=Person("B",15)
 print(p3.info())
+
+
+class Student(Person):
+    def __init__(self,university):
+        Person.__init__(self)
+        self.university=university
+    
+    def info(self):
+        print(f"Hello, I am {self.name} and I am {self.age} years old! I am studying on the university: {self.university}")
